4e6dd6d2099818eb000148a3905ae6a86a545272
weighne <60398449+weighne@users.noreply.github.com>
2023-08-25 16:53:08 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,15 +1,25 @@
 import requests  # for getting the data from the web
 
 
-def deg_to_cardinal(deg):
+def deg_to_cardinal(deg):  # stolen from https://gist.github.com/RobertSudwarts/acf8df23a16afdb5837f
     dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
     ix = round(deg / (360. / len(dirs)))
     return dirs[ix % len(dirs)]
 
 
+def clouds(cloud_data):
+    cloud_types = {"FEW":"Mostly Sunny",
+                   "SCT":"Partly Cloudy",
+                   "BKN":"Mostly Cloudy",
+                   "OVC":"Cloudy"}
+    for key, value in cloud_types.items():
+        if key == cloud_data[:3]:
+            return value
+        else: continue
+
 def get_wind(wind_data):
     dir = deg_to_cardinal(int(wind_data[0:3]))
-    speed = wind_data[3:5]
+    speed = wind_data[3:]
 
     return dir, speed
 
@@ -36,13 +46,15 @@ atmo_pres = ""
 for x in weather:
 	if "/" in x:  # temp and dewpoint
 		try:
-			temp = x.split("/")[0]
-			dewpoint = x.split("/")[1]
+			temp = x.split("/")[0].replace("M","-")
+			dewpoint = x.split("/")[1].replace("M","-")
 		except IndexError:
 			temp = x
 	elif "KT" in x:
 		wind_dir, wind_speed = get_wind(x)
+	elif "FEW" in x or "SCT" in x or "BKN" in x or "OVC" in x:
+	    sky_cover = clouds(x)
 
 print(weather)
 
-print(f"Temp: {temp}\nDewpoint: {dewpoint}\nWindDir: {wind_dir}\nWindSpeed: {wind_speed}")
+print(f"Temp: {temp}\nDewpoint: {dewpoint}\nWindDir: {wind_dir}\nWindSpeed: {wind_speed}\nSky: {sky_cover}")
